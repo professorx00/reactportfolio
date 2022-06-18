38caea0a5939afef0d4c4175f987aa0594cc1228
David Ward <professorx00@gmail.com>
2022-06-18 01:46:16 -0400

    Updated with new work experience and new skills

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -4,7 +4,6 @@ function AboutMe() {
 
   return (
     <div className="container mt-5">
-      
       <div className="row align-items-center border-me">
         <div className="col-md-6">
           <img src="/me.png" alt={"Dave"} className="img-fluid" />
@@ -12,66 +11,207 @@ function AboutMe() {
         <div className="col-md-4">
           <div className="row justify-content-center">
             <div className="card text-white bg-success cardWidth">
-              <div className="card-header text-white"><a id="AboutMe" className="text-white" href="#AboutMe">Dave Ward</a></div>
+              <div className="card-header text-white">
+                <a id="AboutMe" className="text-white" href="#AboutMe">
+                  Dave Ward
+                </a>
+              </div>
               <div className="card-body">
-                <h4 className="card-title">Full Stack Developer</h4>
-                <p className="card-text">Node JS, Express JS, Passport JS,HTML 5, Handlebars</p>
-                <a href="#Portfolio" className="btn btn-warning">See Portfolio</a>
+                <h4 className="card-title">React Developer</h4>
+                <p className="card-text">
+                  React JS, React Native, Node JS, Express JS, Passport JS,HTML
+                  5, Handlebars, Firebase
+                </p>
+                <a href="#Portfolio" className="btn btn-warning">
+                  See Portfolio
+                </a>
               </div>
             </div>
           </div>
         </div>
-
       </div>
       <div className="row ">
         <div className="col-md-6 mb-2">
-          <div className="progress mt-2 " name="Javascript" style={{height: '2rem'}}>
-            <div className="progress-bar bg-success BarText" role="progressbar" style={{ width: '80%' }} aria-valuenow="25"
-              aria-valuemin="0" aria-valuemax="100">Javascript</div>
+          <div
+            className="progress mt-2 "
+            name="Javascript"
+            style={{ height: "2rem" }}
+          >
+            <div
+              className="progress-bar bg-success BarText"
+              role="progressbar"
+              style={{ width: "90%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              Javascript
+            </div>
           </div>
-          <div className="progress mt-2 " name="Express JS" style={{height: '2rem'}}>
-            <div className="progress-bar bg-info BarText" role="progressbar" style={{ width: '50%' }} aria-valuenow="50" aria-valuemin="0"
-              aria-valuemax="100">Express JS</div>
+          <div
+            className="progress mt-2 "
+            name="Express JS"
+            style={{ height: "2rem" }}
+          >
+            <div
+              className="progress-bar bg-info BarText"
+              role="progressbar"
+              style={{ width: "50%" }}
+              aria-valuenow="50"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              Express JS
+            </div>
           </div>
-          <div className="progress mt-2 " name="NodeJS" style={{height: '2rem'}}>
-            <div className="progress-bar bg-danger BarText" role="progressbar" style={{ width: '80%' }} aria-valuenow="80"
-              aria-valuemin="0" aria-valuemax="100">Node JS</div>
+          <div
+            className="progress mt-2 "
+            name="NodeJS"
+            style={{ height: "2rem" }}
+          >
+            <div
+              className="progress-bar bg-danger BarText"
+              role="progressbar"
+              style={{ width: "80%" }}
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              Node JS
+            </div>
           </div>
-          <div className="progress mt-2 " name="Python" style={{height: '2rem'}}>
-            <div className="progress-bar bg-warning BarText" role="progressbar" style={{ width: '55%' }} aria-valuenow="55"
-              aria-valuemin="0" aria-valuemax="100">Python</div>
+          <div
+            className="progress mt-2 "
+            name="Python"
+            style={{ height: "2rem" }}
+          >
+            <div
+              className="progress-bar bg-warning BarText"
+              role="progressbar"
+              style={{ width: "55%" }}
+              aria-valuenow="55"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              Python
+            </div>
           </div>
-          <div className="progress mt-2 " name="C#" style={{height: '2rem'}}>
-            <div className="progress-bar bg-success BarText" role="progressbar" style={{ width: '70%' }} aria-valuenow="80"
-              aria-valuemin="0" aria-valuemax="100">C#</div>
+          <div className="progress mt-2 " name="C#" style={{ height: "2rem" }}>
+            <div
+              className="progress-bar bg-success BarText"
+              role="progressbar"
+              style={{ width: "70%" }}
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              React Native
+            </div>
           </div>
-          <div className="progress mt-2 " name="GIT" style={{height: '2rem'}}>
-            <div className="progress-bar bg-info BarText" role="progressbar" style={{ width: '70%' }} aria-valuenow="70"
-              aria-valuemin="0" aria-valuemax="100">Microsoft SQL</div>
+          <div className="progress mt-2 " name="GIT" style={{ height: "2rem" }}>
+            <div
+              className="progress-bar bg-info BarText"
+              role="progressbar"
+              style={{ width: "70%" }}
+              aria-valuenow="70"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              Microsoft SQL
+            </div>
           </div>
         </div>
         <div className="col-md-6 mb-2">
-            <div className="progress mt-2 " name="MySql" style={{height: '2rem'}}>
-                <div className="progress-bar bg-danger BarText" role="progressbar" style={{ width: '90%' }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">MySQL</div>
+          <div
+            className="progress mt-2 "
+            name="MySql"
+            style={{ height: "2rem" }}
+          >
+            <div
+              className="progress-bar bg-danger BarText"
+              role="progressbar"
+              style={{ width: "90%" }}
+              aria-valuenow="90"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              MySQL
             </div>
-            <div className="progress mt-2 " name="Blender" style={{height: '2rem'}}>
-              <div className="progress-bar bg-warning BarText" role="progressbar" style={{ width: '55%'}} aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">Blender</div>
+          </div>
+          <div
+            className="progress mt-2 "
+            name="Blender"
+            style={{ height: "2rem" }}
+          >
+            <div
+              className="progress-bar bg-warning BarText"
+              role="progressbar"
+              style={{ width: "55%" }}
+              aria-valuenow="55"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              Blender
             </div>
-          
-            <div className="progress mt-2 " name="Photoshop" style={{height: '2rem'}}>
-              <div className="progress-bar bg-success BarText" role="progressbar" style={{ width: '70%' }} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">Photoshop</div>
+          </div>
+
+          <div
+            className="progress mt-2 "
+            name="Photoshop"
+            style={{ height: "2rem" }}
+          >
+            <div
+              className="progress-bar bg-success BarText"
+              role="progressbar"
+              style={{ width: "70%" }}
+              aria-valuenow="70"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              Photoshop
             </div>
-            <div className="progress mt-2 " name="Illustrator" style={{height: '2rem'}}>
-              <div className="progress-bar bg-info BarText" role="progressbar" style={{ width: '60%' }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">Illustrator</div>
+          </div>
+          <div
+            className="progress mt-2 "
+            name="Illustrator"
+            style={{ height: "2rem" }}
+          >
+            <div
+              className="progress-bar bg-info BarText"
+              role="progressbar"
+              style={{ width: "60%" }}
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              Illustrator
             </div>
-            <div className="progress mt-2 " name="GIT" style={{height: '2rem'}}>
-              <div className="progress-bar bg-danger BarText" role="progressbar" style={{ width: '80%' }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">GIT</div>
+          </div>
+          <div className="progress mt-2 " name="GIT" style={{ height: "2rem" }}>
+            <div
+              className="progress-bar bg-danger BarText"
+              role="progressbar"
+              style={{ width: "80%" }}
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              GIT
             </div>
-            
-            <div className="progress mt-2 " name="GIT" style={{height: '2rem'}}>
-              <div className="progress-bar bg-warning BarText" role="progressbar" style={{ width: '90%' }} aria-valuenow="90"
-                aria-valuemin="0" aria-valuemax="100">Visual Code</div>
+          </div>
+
+          <div className="progress mt-2 " name="GIT" style={{ height: "2rem" }}>
+            <div
+              className="progress-bar bg-warning BarText"
+              role="progressbar"
+              style={{ width: "90%" }}
+              aria-valuenow="90"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              React
             </div>
+          </div>
         </div>
       </div>
       <div className="row mt-2">
@@ -79,7 +219,21 @@ function AboutMe() {
           <div className="card border-warning mb-3 cardWidth">
             <div className="card-header">The Start</div>
             <div className="card-body">
-              <p className="card-text"> I started this journey in California. I went to school for animation. I fell in love with the creativity and problem solving of the physics of animation. As my education moved into the 3d realm, I discovered python as a scripting language. I took to it as a bug to a flame. This helped with my previous job as a Implementor for a local time and attendance reseller. As I got use to writing calls in python to the Time and Attendance software, I discovered as I dived deeper into the API that I really need to build a front end for our clients to access data outside of the software on occasion. I decided to seek more education. I joined UNH Coding Boot Camp for Full Stack Development.</p>
+              <p className="card-text">
+                {" "}
+                I started this journey in California. I went to school for
+                animation. I fell in love with the creativity and problem
+                solving of the physics of animation. As my education moved into
+                the 3d realm, I discovered python as a scripting language. I
+                took to it as a bug to a flame. This helped with my previous job
+                as a Implementor for a local time and attendance reseller. As I
+                got use to writing calls in python to the Time and Attendance
+                software, I discovered as I dived deeper into the API that I
+                really need to build a front end for our clients to access data
+                outside of the software on occasion. I decided to seek more
+                education. I joined UNH Coding Boot Camp for Full Stack
+                Development.
+              </p>
             </div>
           </div>
         </div>
@@ -87,7 +241,12 @@ function AboutMe() {
           <div className="card border-info mb-3 cardWidth">
             <div className="card-header">Currently</div>
             <div className="card-body">
-              <p className="card-text">Currently I am working for Tyler Technologies doing C# programming for their DevOps team. I am not sure the C# is my language. There are things about it I love such as the great SQL integration and PowerShell integration. I enjoy my work more in JavaScript and Python. C# seem antiquated by the standards of Node JS, Next JS, Python, That being said I only maintain 4.6 framework. I am sure .net core and new .net 5 sound more of what I want but I don’t foresee my current job leading into those version of C#</p>
+              <p className="card-text">
+                I am working a Farmhand Automation creating a React application
+                to handle fleet management and remote connection to the robotic
+                farm bot. I also worked on developing a react native app for
+                clients to manage their farms and robots.{" "}
+              </p>
             </div>
           </div>
         </div>
@@ -95,13 +254,17 @@ function AboutMe() {
           <div className="card border-success mb-3 cardWidth">
             <div className="card-header">Hopeful</div>
             <div className="card-body">
-              <p className="card-text">I want to get back to UX programming and web based programming. So I am looking at finding new opportunities that will keep up with modern trends such Next JS and Angular. Things change in technology and I am not sure my current job is moving with the tide. Being government based they tend to stick to older technologies. </p>
+              <p className="card-text">
+                I want to advance my knowleadge and experience in the React
+                workflow. I want develop my unit testing. I feel unit testing
+                will make my programming better.{" "}
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default AboutMe;
--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -3,28 +3,82 @@ import React from 'react';
 function Experience() {
   return (
     <div className="container text-left">
-      
       <div className="row">
         <div className="col-md-12 text-center bg-info Contact">
-          <h1><a id="Experience" href="#Experience" className="text-white">Experience</a></h1><a href="#Top" className="back">back to top</a>
+          <h1>
+            <a id="Experience" href="#Experience" className="text-white">
+              Experience
+            </a>
+          </h1>
+          <a href="#Top" className="back">
+            back to top
+          </a>
         </div>
       </div>
       <div className="row">
         <div className="col-md-6">
-          <h3><u>Work Experience:</u></h3>
+          <h3>
+            <u>Work Experience:</u>
+          </h3>
           <ul>
-          <li>
+            <li>
               <ul>
-                <li><strong><u>
-                  <h5>DevOps Engineer</h5>
-                </u></strong></li>
-                <li>Tyler Technologies</li>
-                <li>January 2020 - Present</li>
+                <li>
+                  <strong>
+                    <u>
+                      <h5>React Developer</h5>
+                    </u>
+                  </strong>
+                </li>
+                <li>Farmhand Automation</li>
+                <li>October 2021 - Present</li>
                 <li>
                   <ul>
-                    <li>Maintaining and Supporting C# 4.6 Framework Applications for internal use</li>
-                    <li>Maintaining, Creating, and Supporting CI Builds in Team City and Azure DevOps</li>
-                    <li>Writing PowerShell scripts to enhance the DevOps Processes such as User Identification in LDAP environments</li>
+                    <li>
+                      Develop a client facing react native application with
+                      bluetooth connection to a robotic rover which would allow
+                      for control, reporting, and mapping.
+                    </li>
+                    <li>
+                      Develop a client facing react native application with
+                      bluetooth connection to a robotic rover which would allow
+                      for control, reporting, and mapping.
+                    </li>
+                    <li>
+                      Handle creation of websocket messaging manager, used to
+                      create a connection with the robotic rover.
+                    </li>
+                    <li></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <ul>
+                <li>
+                  <strong>
+                    <u>
+                      <h5>DevOps Engineer</h5>
+                    </u>
+                  </strong>
+                </li>
+                <li>Tyler Technologies</li>
+                <li>January 2020 - October 2021</li>
+                <li>
+                  <ul>
+                    <li>
+                      Maintaining and Supporting C# 4.6 Framework Applications
+                      for internal use
+                    </li>
+                    <li>
+                      Maintaining, Creating, and Supporting CI Builds in Team
+                      City and Azure DevOps
+                    </li>
+                    <li>
+                      Writing PowerShell scripts to enhance the DevOps Processes
+                      such as User Identification in LDAP environments
+                    </li>
                     <li></li>
                   </ul>
                 </li>
@@ -32,17 +86,28 @@ function Experience() {
             </li>
             <li>
               <ul>
-                <li><strong><u>
-                  <h5>Implementation Software Engineer</h5>
-                </u></strong></li>
+                <li>
+                  <strong>
+                    <u>
+                      <h5>Implementation Software Engineer</h5>
+                    </u>
+                  </strong>
+                </li>
                 <li>Cincinnati Time Recorder of Maine (CTR Maine)</li>
                 <li>March 2015 - January 2020</li>
                 <li>
                   <ul>
-                    <li>Writing SQL Queries for custom reports that clients need</li>
+                    <li>
+                      Writing SQL Queries for custom reports that clients need
+                    </li>
                     <li>Client Relations and Rententions</li>
-                    <li>Python Scripts to convert file formats into compatiable formats</li>
-                    <li>Node JS application using Electron for desktop application</li>
+                    <li>
+                      Python Scripts to convert file formats into compatiable
+                      formats
+                    </li>
+                    <li>
+                      Node JS application using Electron for desktop application
+                    </li>
                   </ul>
                 </li>
               </ul>
@@ -50,9 +115,13 @@ function Experience() {
             <br />
             <li>
               <ul>
-                <li><strong><u>
-                  <h5>Broadband Technician Support Respentive</h5>
-                </u></strong></li>
+                <li>
+                  <strong>
+                    <u>
+                      <h5>Broadband Technician Support Respentive</h5>
+                    </u>
+                  </strong>
+                </li>
                 <li>Time Warner Cable</li>
                 <li>April 2011 - January 2015</li>
                 <li>
@@ -67,9 +136,13 @@ function Experience() {
             <br />
             <li>
               <ul>
-                <li><strong><u>
-                  <h5>Sales Manager and Computer Technician</h5>
-                </u></strong></li>
+                <li>
+                  <strong>
+                    <u>
+                      <h5>Sales Manager and Computer Technician</h5>
+                    </u>
+                  </strong>
+                </li>
                 <li>Staples</li>
                 <li>September 2001 - January 2011</li>
                 <li>
@@ -84,63 +157,92 @@ function Experience() {
               </ul>
             </li>
             <br />
-            
           </ul>
         </div>
         <div className="col-md-6">
           <ul>
             <li>
-                <ul>
-                  <li><u><strong>
-                    <h5>Intern</h5>
-                  </strong></u></li>
-                  <li>Motion Analysis Studios</li>
-                  <li>June 2004 - September 2004</li>
-                  <li>
-                    <ul>
-                      <li>Work closely with Technical director to set cameras</li>
-                      <li>Run "Motion Builder" while capturing and adjusting the skelton</li>
-                      <li>Apply Rig (skeleton) to model for real time capture</li>
-                      <li>Clean up motion data after capturing</li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
+              <ul>
+                <li>
+                  <u>
+                    <strong>
+                      <h5>Intern</h5>
+                    </strong>
+                  </u>
+                </li>
+                <li>Motion Analysis Studios</li>
+                <li>June 2004 - September 2004</li>
+                <li>
+                  <ul>
+                    <li>Work closely with Technical director to set cameras</li>
+                    <li>
+                      Run "Motion Builder" while capturing and adjusting the
+                      skelton
+                    </li>
+                    <li>Apply Rig (skeleton) to model for real time capture</li>
+                    <li>Clean up motion data after capturing</li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
           </ul>
-          <h3><u>Education:</u></h3>
+          <h3>
+            <u>Education:</u>
+          </h3>
           <ul>
             <li>
               <ul>
-              <u><strong><li>University of New Hampshire Coding Bootcamp</li></strong></u>
+                <u>
+                  <strong>
+                    <li>University of New Hampshire Coding Bootcamp</li>
+                  </strong>
+                </u>
                 <li>June 2019 - December 2019</li>
-                <li>This 24-week program is for working professionals interested in web development or who are actively
-                  seeking a career change or advancement. Even better, you can work full-time while you learn the skills to
-              become a professional web developer.</li>
+                <li>
+                  This 24-week program is for working professionals interested
+                  in web development or who are actively seeking a career change
+                  or advancement. Even better, you can work full-time while you
+                  learn the skills to become a professional web developer.
+                </li>
               </ul>
             </li>
             <br />
             <li>
               <ul>
-              <u><strong><li>Character Animation Certification</li> </strong></u>
+                <u>
+                  <strong>
+                    <li>Character Animation Certification</li>{" "}
+                  </strong>
+                </u>
                 <li>Animation Mentor</li>
                 <li>June 2007 - December 2009</li>
-                <li>A 2 year program on character animation. Being taught by animators from pixar, disney, blue sky</li>
+                <li>
+                  A 2 year program on character animation. Being taught by
+                  animators from pixar, disney, blue sky
+                </li>
               </ul>
             </li>
             <br />
             <li>
               <ul>
-                <u><strong><li>Associates of Sciences, Animation</li></strong></u>
+                <u>
+                  <strong>
+                    <li>Associates of Sciences, Animation</li>
+                  </strong>
+                </u>
                 <li>Brooks College</li>
                 <li>June 2004 - December 2006</li>
-                <li>A 2 year program on character animation. Activities:Animation Club, Life drawing work shops</li>
+                <li>
+                  A 2 year program on character animation. Activities:Animation
+                  Club, Life drawing work shops
+                </li>
               </ul>
             </li>
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Experience;
